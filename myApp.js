4d05b50fa6e68587ce1d0c04da7ebdfa17d7d0b2
Emilea Chin <emileachinn@gmail.com>
2025-05-22 06:01:12 +0000

    step 2 (express)

--- myApp.js
+++ myApp.js
@@ -3,10 +3,16 @@ let app = express();
 
 console.log("Hello World!");
 
+/*
 app.get('/', function(req, res) {
     res.send('Hello Express');
 })
+*/
 
+app.get('/', function(req, res) {
+    let absolutePath = __dirname + "/views/index.html"
+    res.sendFile(absolutePath)
+})
 
 
 
