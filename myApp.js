0e7be960133edd7ae5330e3a6d0494fa08067a00
Emilea Chin <emileachinn@gmail.com>
2025-05-23 02:32:41 +0000

    part 6 (express) (2)

--- myApp.js
+++ myApp.js
@@ -20,7 +20,7 @@ app.use('/public', express.static(__dirname + '/public'))
 
 app.get('/json', function(req, res) {
     let message = "Hello json"
-    if (process.env.MESSAGE_STYLE === "uppercase") {
+    if (process.env.MESSAGE_STYLE == "uppercase") {
         message = "HELLO JSON"
     }
     res.json({"message": message})
